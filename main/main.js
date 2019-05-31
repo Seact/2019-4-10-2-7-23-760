cebfc18cfc7b1672b3e56ea7258ec1d98652ad1d
Seact <seact_l@outlook.com>
2019-05-31 22:47:03 +0800

    ''

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+  return Math.round(
+    inputs.parkTime * 0.25 + (
+      inputs.distance <= 2
+      ? 6
+      : inputs.distance <= 8
+      ? (inputs.distance * 0.8 + 4.4)
+      : (inputs.distance * 1.2 + 1.2)
+    )
+  );
 };
